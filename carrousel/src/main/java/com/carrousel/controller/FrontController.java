a780bfb87b142203fb6d8714ba8dfa52738ff96c
Guillaume Jacquet <guillaume.jacquet@capgemini.com>
2020-02-14 16:40:04 +0100

    filtre

--- carrousel/src/main/java/com/carrousel/controller/FrontController.java
+++ carrousel/src/main/java/com/carrousel/controller/FrontController.java
@@ -1,7 +1,10 @@
 package com.carrousel.controller;
 
 import java.io.File;
-import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.activation.MimetypesFileTypeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,23 +16,40 @@ import com.carrousel.properties.ApplicationProperties;
 @Controller
 public class FrontController {
 
-    @Autowired
-    ApplicationProperties applicationProperties;
+	@Autowired
+	ApplicationProperties applicationProperties;
 
-    @GetMapping( "/" )
-    public String homePage( final Model model ) throws InterruptedException {
+	@GetMapping("/")
+	public String homePage(final Model model) {
 
-        String chemin = applicationProperties.getImages();
-        File[] fichiers = {};
-        File repertoire = new File( chemin );
-        if ( repertoire.isDirectory() ) {
-            fichiers = repertoire.listFiles();
-        }
+		String chemin = applicationProperties.getImages();
+		List<File> fichiers = new ArrayList<>();
+		File repertoire = new File(chemin);
+		if (repertoire.isDirectory()) {
+			fichiers = choisirFichiers(repertoire);
+		}
 
-        model.addAttribute( "chemin", chemin );
-        model.addAttribute( "fichiers", fichiers );
+		model.addAttribute("chemin", chemin);
+		model.addAttribute("fichiers", fichiers);
 
-        return "home.html";
-    }
+		return "home.html";
+	}
+
+	private List<File> choisirFichiers(File repertoire) {
+		List<File> fichiers = new ArrayList<>();
+		for (File fichier : repertoire.listFiles()) {
+			if (!fichier.isDirectory() && isImage(fichier)) {
+				fichiers.add(fichier);
+			}
+		}
+
+		return fichiers;
+	}
+
+	private boolean isImage(File fichier) {
+		String mimeType = new MimetypesFileTypeMap().getContentType(fichier);
+		String type = mimeType.split("/")[0];
+		return type.equals("image");
+	}
 
 }
