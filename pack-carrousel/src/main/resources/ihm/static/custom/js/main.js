23c93ac8eab1c0e8e583b9125c8c69d727eb43e3
Guillaume Jacquet <guiguillaume.jacquet@gmail.com>
2020-02-14 13:32:51 +0100

    js

--- pack-carrousel/src/main/resources/ihm/static/custom/js/main.js
+++ pack-carrousel/src/main/resources/ihm/static/custom/js/main.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-	console.log("JS Chargé !");
-
 	console.log("Rechargement de la page toutes les 10 min");
 	setTimeout(function() {
 		location.reload();
 	}, 600000);
 	
-	console.log("Défilement des images toutes les 5 secondes");
+	console.log("Defilement des images toutes les 5 secondes");
 	$('.carousel').carousel({
 	  interval: 5000
 	})
