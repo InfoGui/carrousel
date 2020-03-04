43b932f5aa709690acbdd3d192c9d2b67b09b5fc
PTRT11901 <guillaume.jacquet@capgemini.com>
2020-03-04 17:18:23 +0100

    test

--- carrousel/src/test/java/com/carrousel/utils/FilteUtilsTest.java
+++ carrousel/src/test/java/com/carrousel/utils/FilteUtilsTest.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,16 +23,18 @@ public class FilteUtilsTest {
 	@Autowired
 	ApplicationProperties applicationProperties;
 
+	@Tag("dao")
 	@DisplayName("getImagesTest")
 	@Test
 	public void getImagesTest() {
 		File repertoire = new File(applicationProperties.getImages());
 		List<File> images = fileUtils.getImages(repertoire);
-
+		
 		assertEquals(2, images.size());
-		assertEquals("image1.png", images.get(0).getName());
+		assertEquals("image1.jpg", images.get(0).getName());
 	}
 
+	@Tag("algo")
 	@DisplayName("getIframesTest")
 	@Test
 	public void getIframesTest() {
