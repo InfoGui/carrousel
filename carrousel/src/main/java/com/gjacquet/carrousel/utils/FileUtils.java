ba39cdb86f8d3486dfb54dc68e66d681dc3ba4b0
PTRT11901 <guillaume.jacquet@capgemini.com>
2020-03-05 17:17:52 +0100

    ordre des fichier

--- carrousel/src/main/java/com/gjacquet/carrousel/utils/FileUtils.java
+++ carrousel/src/main/java/com/gjacquet/carrousel/utils/FileUtils.java
@@ -1,12 +1,13 @@
 package com.gjacquet.carrousel.utils;
 
 import java.io.File;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -18,19 +19,25 @@ import org.springframework.stereotype.Service;
 public class FileUtils {
 
 	public List<File> getImages(File repertoire) {
-		List<File> fichiers = new ArrayList<>();
+		List<File> fichiers = Arrays.asList(repertoire.listFiles());
+		Collections.sort(fichiers);
+
+		List<File> images = new ArrayList<>();
 		for (File fichier : repertoire.listFiles()) {
 			if (!fichier.isDirectory() && isImage(fichier)) {
-				fichiers.add(fichier);
+				images.add(fichier);
 			}
 		}
 
-		return fichiers;
+		return images;
 	}
 
 	public List<String> getIframes(File repertoire) {
+		List<File> fichiers = Arrays.asList(repertoire.listFiles());
+		Collections.sort(fichiers);
+
 		List<String> iframes = new ArrayList<>();
-		for (File fichier : repertoire.listFiles()) {
+		for (File fichier : fichiers) {
 			if (!fichier.isDirectory() && isIframe(fichier)) {
 				StringBuilder contentBuilder = new StringBuilder();
 				try (Stream<String> stream = Files.lines(Paths.get(fichier.getAbsolutePath()),
